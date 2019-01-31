e79765d68a929d2b8d54be0685e64161eb5d382c
Arthur A. Bergamaschi arthurbergmz@gmail.com <arthurbergmz@gmail.com>
2019-01-31 17:38:12 -0200

    :construction: Update dataset selection modal.

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,87 +1,37 @@
-$(document).ready(function () {
+const bodyElement = document.body
 
-  var x = $('.smartlink')
-  
-  $.each(x, function(i, val) {
-    var ref = $(this);
-    var ref_top = ref.offset().top;
-    var ref_id = ref.attr('class').slice(-3); // this needs to be more robust
+const utils = {
+  lockBody (lock) {
+    if (lock) {
+      if (!bodyElement.classList.contains('lock')) bodyElement.classList.add('lock')
+    } else {
+      bodyElement.classList.remove('lock')
+    }
+  },
+  isBodyLocked () {
+    return bodyElement.classList.contains('lock')
+  }
+}
 
-    var content_top = $('.prose__content').offset().top;
-
-    var text_ref = $('.smartlink-content--' + ref_id );
-    text_ref.css({top: ref_top - content_top });
-  });
-
-  $('.smartlink').click(function(){
-    var ref_id = $(this).attr('class').slice(-3);
-    $('.smartlink-content--' + ref_id ).toggleClass('js-active');
-  });
-
-  // Smooth Scrolling Function
-  $('a[href*=#]:not([href=#])').click(function () {
-      var $targ = $(this.hash),
-          host1 = this.hostname,
-          host2 = location.hostname,
-          path1 = this.pathname.replace(/^\//, ''),
-          path2 = location.pathname.replace(/^\//, '');
-
-      if (!$targ.length) {
-          $targ = $('[name=' + this.hash.slice(1) + ']');
-      }
-
-      if ($targ.length && (host1 === host2 || path1 === path2)) {
-          $('html, body').animate({ scrollTop: $targ.offset().top }, 1000);
-
-          return false;
-      }
-
-      return true;
-  });
-
-
-  $('.js-submit').click(function() {
-    $('.js-modal').addClass('js-active');
-    $('.js-overlay').addClass('js-active');
-  });
-  
-  $('.js-overlay').click(function() {
-    $('.js-modal').removeClass('js-active'); // FIX ME!!!
-    $(this).removeClass('js-active'); // FIX ME!!!
-    $('.js-result').removeClass('js-active');
-  });
-
-  $('.js-comment-trigger').click(function() {
-    $('.js-result').addClass('js-active'); // FIX ME!!!
-  });
-
-  
-  // $( '.fullscreen-section:in-viewport( 100 )' ).addClass('fullscreen-section--in-viewport');
-
-  // $(window).scroll(function() {
-  //   $( '.fullscreen-section' ).removeClass('fullscreen-section--in-viewport');
-  //   $( '.fullscreen-section:in-viewport( 100 )' ).addClass('fullscreen-section--in-viewport');
-  // });
-
-  // window.sr = ScrollReveal();
-  
-  // sr.reveal('.fadein');
-  // sr.reveal('.fadein--from-right', { duration: 1400 });
-
-  window.sr = ScrollReveal({ 
-    reset: false,
-    duration: 1200, 
-    distance: '20px',
-    scale: 0.95,
-    viewFactor: 0.3,
-    delay: 50
-  });
-
-  sr.reveal('.fadein--from-right', { origin: 'right' });
-  sr.reveal('.fadein--from-left', { origin: 'left' });
-  sr.reveal('.fadein--from-bottom', { origin: 'bottom' });
-  sr.reveal('.fadein--from-top', { origin: 'top' });
-  
-
-
-}); // doc.ready
+// CHOOSE DATASET
+const chooseDataSetButton = document.getElementsByClassName('js-choose-dataset-btn')[0]
+const chooseDataSetWindow = document.getElementsByClassName('js-homepage-dataset-selection-window')[0]
+const chooseDataSetCloseButton = document.getElementsByClassName('js-homepage-dataset-selection-window-close')[0]
+chooseDataSetButton.addEventListener('click', () => {
+  chooseDataSetWindow.classList.toggle('hidden')
+  utils.lockBody(!utils.isBodyLocked())
+})
+chooseDataSetWindow.addEventListener('click', (e) => {
+  console.log(e.target)
+  if (e.target.classList.contains('js-homepage-dataset-selection-window') &&
+    !chooseDataSetWindow.classList.contains('hidden')) {
+    chooseDataSetWindow.classList.add('hidden')
+    utils.lockBody(false)
+  }
+})
+chooseDataSetCloseButton.addEventListener('click', () => {
+  if (!chooseDataSetWindow.classList.contains('hidden')) {
+    chooseDataSetWindow.classList.add('hidden')
+    utils.lockBody(false)
+  }
+})
